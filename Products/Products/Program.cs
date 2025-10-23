26d80eef383d9383823226b1cd189c2dce29ff31
ARI\ravish.kumar <ravish.kumar@leadventure.com>
2025-10-23 19:01:19 +0530

    feat: add Dockerfile and update docker-compose for Products microservice

--- Products/Products/Program.cs
+++ Products/Products/Program.cs
@@ -40,11 +40,10 @@ namespace Products
             var app = builder.Build();
 
             // Configure the HTTP request pipeline
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
 
             app.UseHttpsRedirection();
             app.UseAuthorization();
